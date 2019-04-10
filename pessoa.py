7a5d8bc5092969e4d61c1da2cb35067a4599072c
Natália Zaramelo <zaramelo@gmail.com>
2019-04-10 13:37:17 -0300

    Criado o método cumprimentar

--- pessoa.py
+++ pessoa.py
@@ -1,7 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=34):
+        self.nome = nome
+        self.idade = idade
     def cumprimentar(self):
-        return 'Olá'
+        return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Natalia')
     print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Natália'
+    print(p.nome)
