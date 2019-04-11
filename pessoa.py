338c981f6e599d22d2eae595f4f462daa7c5d960
Natália Zaramelo <zaramelo@gmail.com>
2019-04-11 12:02:11 -0300

    Criado o atributo de classe - metodo estatico e metodo classe

--- pessoa.py
+++ pessoa.py
@@ -8,6 +8,16 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod #metodo de classe o conceito é paracido com os atridutos de classe, está atrelado a classe e independe de qul objeto está sendo executado
+    def metodo_estatico(): #nao é neccessário receber nenhum atributo, esse astaticmethod é chamado de decoretion
+        return 42 #neste caso a logica deve fazer algum calculo que independa da classe quanto do objeto
+
+    @classmethod # outro decoretion metodo de classe
+    def nome_e_atributos_de_classe(cls):#neste caso vamos precisar de acesso ao metodo, esse cls faz alusão a class que neste caso é Pessoa
+        return f'(cls) - olhos{cls.olhos}'
+
+#não confundir metodos e atributos de classe com metodos e atributos de instacia
+
 if __name__ == '__main__':
     natalia = Pessoa(nome='Natalia') #objeto complexo do tipo pessoa tem nome e está sendo passado como atributo para objeto junior
     junior = Pessoa(natalia, nome='Junior') #natalia seria filho de junior
@@ -32,3 +42,5 @@ if __name__ == '__main__':
         print(natalia.__dict__)#__dict__possui referencia apenas para os atributo de instância, portanto quando você insere o atributo no objeto ele passa a fazer parte do objeto
         print(junior.__dict__)
         print(id(Pessoa.olhos), id(junior.olhos), id(natalia.olhos))
+        print(Pessoa.metodo_estatico(), junior.metodo_estatico())
+        print(Pessoa.nome_e_atributos_de_classe(), junior.nome_e_atributos_de_classe())#neste caso é possivel acessar pela calsse Pessoa ou pelo objeto Junior
